cd79f3b4a3a3b893d36db2750f23842f7b68ebfd
Ben Mezger <me@benmezger.nl>
2020-09-06 17:52:24 -0300

    Feat: Generate Handler and set Headers and Content

--- routes.go
+++ routes.go
@@ -1,9 +1,9 @@
 package main
 
 import (
-	"strings"
-
 	"github.com/julienschmidt/httprouter"
+	"net/http"
+	"strings"
 )
 
 func GenerateRoutes(spec *HTTPSpec, handler *httprouter.Router) {
@@ -14,12 +14,23 @@ func GenerateRoutes(spec *HTTPSpec, handler *httprouter.Router) {
 	}
 }
 
+func GenerateHandler(method *HTTPSpecMethod) func(w http.ResponseWriter, r *http.Request, p httprouter.Params) {
+	return func(w http.ResponseWriter, r *http.Request, p httprouter.Params) {
+		for k, v := range method.Response.Header {
+			w.Header().Add(k, v)
+		}
+		w.WriteHeader(method.Response.Status)
+		w.Write([]byte(method.Response.Payload))
+	}
+
+}
+
 func SetupRoutes(spec *HTTPSpec) *httprouter.Router {
 	router := httprouter.New()
 	GenerateRoutes(spec, router)
 	for path, attrs := range spec.Paths {
 		for _, m := range attrs {
-			m.Invoke(router, path, Index)
+			m.Invoke(router, path, GenerateHandler(m))
 		}
 	}
 	return router
